b56af12215f83b13b38ce3f0e0cdbdaa5430ab3a
Diogo Henrique <diogohs110@gmail.com>
2022-12-14 20:56:40 -0300

    Project finished

--- app/js/script.js
+++ app/js/script.js
@@ -1,17 +1,25 @@
+const bodyElement = document.body;
 const toggleBtn = document.querySelector('.header__toggle');
 const header = document.querySelector('.header');
 const headerOverlay = document.querySelector('.overlay');
+const fadeElements = document.querySelectorAll('.has-fade');
+
 
 toggleBtn.addEventListener('click', () => {
-  header.classList.toggle('open')
-
+  header.classList.toggle('open');
+  bodyElement.classList.toggle('noscroll');
 
   // Overlay effect
-  if (header.classList.contains('open')) {
-    headerOverlay.classList.remove('fade-out')
-    headerOverlay.classList.add('fade-in')
-  } else {
-    headerOverlay.classList.remove('fade-in');
-    headerOverlay.classList.add('fade-out');
+  if (header.classList.contains('open')) { //Close hamburger menu
+    fadeElements.forEach(element => {
+      element.classList.remove('fade-out');
+			element.classList.add('fade-in');
+    })
+  } else { //Open hamburger menu
+    fadeElements.forEach(element => {
+      element.classList.remove('fade-in');
+			element.classList.add('fade-out');
+    })
+    
   }
 });
